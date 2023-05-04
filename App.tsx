74831e0b21d15c5c855277d6ca45e68a825e9ae4
anirban00537 <anirban00537@gmail.com>
2023-05-05 00:21:43 +0600

    updated

--- App.tsx
+++ App.tsx
@@ -5,24 +5,17 @@ import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 import {Main} from './navigation/Main';
 import {SafeAreaView} from 'react-native';
-import {createRealmContext} from '@realm/react';
-import {VisionCard} from './db/realm';
 
 const App = () => {
   GoogleSignin.configure({
     webClientId:
       '469012389462-atmhi36bembrin6cq0uh73ucc4aabtfo.apps.googleusercontent.com',
   });
-  const {RealmProvider} = createRealmContext({
-    schema: [VisionCard],
-  });
   return (
     <PaperProvider>
       <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
-          <RealmProvider>
             <Main />
-          </RealmProvider>
         </NavigationContainer>
       </SafeAreaView>
     </PaperProvider>
--- hooks/visionboard.hook.ts
+++ hooks/visionboard.hook.ts
@@ -1,23 +1,52 @@
-import Realm from 'realm';
-import {useEffect, useState} from 'react';
-import { VisionCard} from '../db/realm';
 import {useNavigation, useIsFocused} from '@react-navigation/native';
+import {useEffect, useState} from 'react';
+//@ts-ignore
+import SQLite from 'react-native-sqlite-storage';
+
+const db = SQLite.openDatabase({
+  name: 'mydb.db',
+  location: 'default',
+});
 
 export const useVisionBoardCreate = () => {
   const navigation = useNavigation();
-  const createVisionBoard = async (title: string, description: string) => {
-    const realm = await Realm.open({schema: [VisionCard.schema]});
+  db.transaction((tx: any) => {
+    tx.executeSql(
+      'CREATE TABLE IF NOT EXISTS visionBoards (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)',
+      [],
+      (tx: any, result: any) => {
+        console.log('VisionBoards table created successfully.');
+      },
+      (error: any) => {
+        console.error('Error creating VisionBoards table: ', error);
+      },
+    );
+  });
 
-    realm.write(() => {
-      realm.create('VisionBoard', {
-        id: new Date().toISOString(),
-        title: title,
-        description: description,
+  const createVisionBoard = async (title: string, description: string) => {
+    try {
+      const sql = 'INSERT INTO visionBoards (title, description) VALUES (?, ?)';
+      const args = [title, description];
+      const result: any = await executeSql(sql, args);
+
+      console.log(`Created new vision board with ID: ${result.insertId}`);
+      navigation.goBack();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const executeSql = (sql: string, args: any[] = []) =>
+    new Promise((resolve, reject) => {
+      db.transaction((tx: any) => {
+        tx.executeSql(
+          sql,
+          args,
+          (tx: any, result: any) => resolve(result),
+          (error: any) => reject(error),
+        );
       });
     });
-
-    console.log('Created new vision board in Realm');
-  };
 
   return {createVisionBoard};
 };
@@ -29,22 +58,46 @@ export const useGetVisionBoard = () => {
   const [error, setError] = useState(null);
 
   const getVisionBoards = async () => {
-    console.log('calling getVisionBoards');
-
+    console.log(
+      'callingggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg',
+    );
+    setLoading(true);
     try {
-      const realm = await Realm.open({schema: [VisionCard.schema]});
-      const visionBoardsData = realm.objects('VisionBoard');
+      const sql = 'SELECT * FROM visionBoards';
+      const results: any = await executeSql(sql);
+
+      const len = results.rows.length;
+      const visionBoardsData = [];
+      for (let i = 0; i < len; i++) {
+        const row = results.rows.item(i);
+        visionBoardsData.push({
+          id: row.id,
+          title: row.title,
+          description: row.description,
+        });
+      }
       setVisionBoards(visionBoardsData);
       setLoading(false);
-    } catch (error: any) {
-      console.error(error);
-      setError(error);
+    } catch (e: any) {
+      console.error(e);
+      setError(e);
       setLoading(false);
     }
   };
 
+  const executeSql = (sql: string, args: any[] = []) =>
+    new Promise((resolve, reject) => {
+      db.transaction((tx: any) => {
+        tx.executeSql(
+          sql,
+          args,
+          (tx: any, result: any) => resolve(result),
+          (error: any) => reject(error),
+        );
+      });
+    });
+
   useEffect(() => {
-    setLoading(true);
     getVisionBoards();
   }, [isFocused]);
 
