379238260657206c16fb97e0e3823651ad9be06f
anirban00537 <anirban00537@gmail.com>
2023-05-04 15:09:00 +0600

    updated

--- hooks/visionboard.hook.ts
+++ hooks/visionboard.hook.ts
@@ -1,25 +1,22 @@
-import firestore from '@react-native-firebase/firestore';
-import {useNavigation, useIsFocused} from '@react-navigation/native';
+import Realm from 'realm';
 import {useEffect, useState} from 'react';
+import { VisionCard} from '../db/realm';
+import {useNavigation, useIsFocused} from '@react-navigation/native';
 
 export const useVisionBoardCreate = () => {
   const navigation = useNavigation();
-
   const createVisionBoard = async (title: string, description: string) => {
-    try {
-      const visionBoardCollection = firestore().collection('visionBoards');
+    const realm = await Realm.open({schema: [VisionCard.schema]});
 
-      // Create a new document in the visionBoards collection
-      const newVisionBoard = await visionBoardCollection.add({
-        title,
-        description,
+    realm.write(() => {
+      realm.create('VisionBoard', {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
       });
+    });
 
-      navigation.goBack();
-      console.log(`Created new vision board with ID: ${newVisionBoard.id}`);
-    } catch (error) {
-      console.error(error);
-    }
+    console.log('Created new vision board in Realm');
   };
 
   return {createVisionBoard};
@@ -30,30 +27,24 @@ export const useGetVisionBoard = () => {
   const isFocused = useIsFocused();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
   const getVisionBoards = async () => {
-    console.log(
-      'callingggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg',
-    );
-    setLoading(true);
+    console.log('calling getVisionBoards');
+
     try {
-      const visionBoardCollection = firestore().collection('visionBoards');
-      const snapshot = await visionBoardCollection.get();
-
-      const visionBoardsData = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
+      const realm = await Realm.open({schema: [VisionCard.schema]});
+      const visionBoardsData = realm.objects('VisionBoard');
       setVisionBoards(visionBoardsData);
       setLoading(false);
-    } catch (e: any) {
-      console.error(e);
-      setError(e);
+    } catch (error: any) {
+      console.error(error);
+      setError(error);
       setLoading(false);
     }
   };
 
   useEffect(() => {
+    setLoading(true);
     getVisionBoards();
   }, [isFocused]);
 
