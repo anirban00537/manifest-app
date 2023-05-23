465fb4a81f6d7634a09b8af006c94b14b367cd4c
anirban00537 <anirban00537@gmail.com>
2023-05-23 15:29:50 +0600

    updated

--- android/app/src/main/java/com/realmtemplate/MainActivity.java
+++ android/app/src/main/java/com/realmtemplate/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ReactActivity {
     }
   @Override
   protected String getMainComponentName() {
-    return "achive-anything-visionboard";
+    return "Visionfest";
   }
 
   /**
